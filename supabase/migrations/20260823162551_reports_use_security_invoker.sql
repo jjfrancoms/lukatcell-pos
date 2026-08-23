0cbab9d1514407dbd7e315395d6c3513bf02d374
jjfrancoms <170184056+jjfrancoms@users.noreply.github.com>
2026-08-23 11:26:03 -0500

    security: run financial reports with invoker rights

---
+++ supabase/migrations/20260823162551_reports_use_security_invoker.sql
@@ -0,0 +1,2 @@
+alter function public.resumen_ganancias(timestamp with time zone, timestamp with time zone) security invoker;
+alter function public.top_productos_ganancia(timestamp with time zone, timestamp with time zone, integer) security invoker;

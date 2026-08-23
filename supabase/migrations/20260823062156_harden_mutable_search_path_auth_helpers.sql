490fe11006b029c127380e8a7fbe5125b05dc9fb
jjfrancoms <170184056+jjfrancoms@users.noreply.github.com>
2026-08-23 01:22:23 -0500

    security: pin auth helper search paths

---
+++ supabase/migrations/20260823062156_harden_mutable_search_path_auth_helpers.sql
@@ -0,0 +1,3 @@
+alter function public.auth_location_id() set search_path = public;
+alter function public.auth_staff_id() set search_path = public;
+alter function public.auth_is_admin() set search_path = public;
